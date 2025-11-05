de9bfeb281218a1e444484978adc7eb27df690c3
Dev Sharma <dev.bs24dmu026@spjain.org>
2025-11-05 13:33:12 +1100

    Refactor plot_gantt function for readability

--- gantt.py
+++ gantt.py
@@ -1,38 +1,17 @@
 import matplotlib.pyplot as plt
 
 def plot_gantt(results, title="Gantt Chart", filename="gantt_chart.png"):
-    """
-    Plots a Gantt chart for given scheduling results.
-    Each process dict should contain: pid, start, finish
-    """
     fig, ax = plt.subplots(figsize=(10, 5))
-
     for r in results:
-        ax.barh(
-            f"P{r['pid']}",
-            r['finish'] - r['start'],
-            left=r['start'],
-            height=0.5,
-            align='center',
-            edgecolor='black'
-        )
-        # Label each bar with start and finish times
-        ax.text(
-            (r['start'] + r['finish']) / 2,
-            f"P{r['pid']}",
-            f"{r['start']}–{r['finish']}",
-            ha='center',
-            va='center',
-            color='white',
-            fontsize=9,
-            fontweight='bold'
-        )
-
+        ax.barh(f"P{r['pid']}", r['finish'] - r['start'], left=r['start'],
+                height=0.5, edgecolor='black', align='center')
+        ax.text((r['start'] + r['finish']) / 2, f"P{r['pid']}",
+                f"{r['start']}–{r['finish']}", ha='center', va='center',
+                color='white', fontsize=9, fontweight='bold')
     ax.set_xlabel("Time")
     ax.set_ylabel("Process")
     ax.set_title(title)
     ax.grid(True, axis='x', linestyle='--', alpha=0.6)
-
     plt.tight_layout()
     plt.savefig(filename)
     plt.show()
